8f6370542a1c4dc6fc6035e6c5521459aae19613
PranayGohil <pranaygohil2@gmail.com>
2025-08-13 17:17:34 +0530

    Dashboard page completed

--- admin/src/views/admin/ChartHorizontal.js
+++ admin/src/views/admin/ChartHorizontal.js
@@ -14,6 +14,71 @@ const ChartHorizontal = ({ weeklyRevenue }) => {
     const revenueData = weeklyRevenue.values || [];
 
 
+    const ExternalTooltip = React.useCallback(({ chart, tooltip }) => {
+        let color = '';
+        let text = '';
+        let value = '';
+        let icon = '';
+        const positionY = chart.canvas.offsetTop;
+        const positionX = chart.canvas.offsetLeft;
+        const { opacity } = tooltip;
+
+        if (tooltipRef.current.opacity === 0) {
+            tooltipRef.current.style.opacity = 0;
+            return;
+        }
+
+        const left = `${positionX + tooltip.dataPoints[0].element.x - 75}px`;
+        const top = `${positionY + tooltip.caretY}px`;
+
+        if (tooltip.body) {
+            const { dataIndex, datasetIndex } = tooltip.dataPoints[0];
+            color = tooltip.labelColors[0].borderColor;
+            text = chart.data.labels[dataIndex].toLocaleUpperCase();
+            value = chart.data.datasets[datasetIndex].data[dataIndex];
+            icon = chart.data.datasets[datasetIndex].icon;
+        }
+
+        tooltipRef.current.classList.remove('above', 'below', 'no-transform');
+        if (tooltip.yAlign) {
+            tooltipRef.current.classList.add(tooltip.yAlign);
+        } else {
+            tooltipRef.current.classList.add('no-transform');
+        }
+
+        tooltipRef.current.style.opacity = opacity;
+        tooltipRef.current.style.left = left;
+        tooltipRef.current.style.top = top;
+
+        ReactDOM.render(
+            <>
+                <div
+                    style={{ borderColor: color, borderWidth: 1, borderStyle: 'solid' }}
+                    className="icon-container  d-flex align-middle align-items-center justify-content-center align-self-center rounded-xl sh-5 sw-5 rounded-xl me-3"
+                >
+                    <CsLineIcons icon={icon} stroke={color} />
+                </div>
+                <div>
+                    <span className="text d-flex align-middle text-alternate align-items-center text-small">{text}</span>
+                    <span className="value d-flex align-middle text-body align-items-center cta-4">{value}</span>
+                </div>
+            </>,
+            tooltipRef.current
+        );
+    }, []);
+
+    const LegendLabels = React.useMemo(() => {
+        return {
+            font: {
+                size: 14,
+                family: themeValues.font,
+            },
+            padding: 20,
+            usePointStyle: true,
+            boxWidth: 10,
+        };
+    }, [themeValues]);
+
     const data = React.useMemo(() => ({
         labels,
         datasets: [
@@ -26,6 +91,75 @@ const ChartHorizontal = ({ weeklyRevenue }) => {
             }
         ],
     }), [labels, revenueData, themeValues]);
+
+    const config = React.useMemo(() => {
+        return {
+            type: 'bar',
+            options: {
+                elements: {
+                    bar: {
+                        borderWidth: 1.5,
+                        borderRadius: parseInt(themeValues.borderRadiusMd, 10),
+                        borderSkipped: false,
+                    },
+                },
+                plugins: {
+                    crosshair: false,
+                    datalabels: false,
+                    legend: {
+                        position: 'bottom',
+                        labels: LegendLabels,
+                    },
+                    tooltip: {
+                        enabled: false,
+                        external: ExternalTooltip,
+                    },
+                    streaming: false,
+                },
+                interaction: {
+                    intersect: true,
+                    mode: 'point',
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    y: {
+                        min: 300,
+                        max: 800,
+                        grid: {
+                            display: true,
+                            lineWidth: 1,
+                            color: themeValues.separatorLight,
+                            drawBorder: false,
+                        },
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 100,
+                            padding: 8,
+                            fontColor: themeValues.alternate,
+                        },
+                    },
+                    x: {
+                        grid: { display: false, drawBorder: false },
+                    },
+                },
+            },
+            data,
+        };
+    }, [data, LegendLabels, ExternalTooltip, themeValues]);
+
+    useEffect(() => {
+        let myChart = null;
+        if (chartContainer && chartContainer.current) {
+            Chart.register(...registerables);
+            myChart = new Chart(chartContainer.current, config);
+        }
+        return () => {
+            if (myChart) {
+                myChart.destroy();
+            }
+        };
+    }, [config]);
 
     return (
         <>
